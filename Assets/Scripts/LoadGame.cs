93216662a6222c66d000f6be9e31c2e5f99a3422
Filipe Ghisi <filipe.ghisi@gmail.com>
2018-06-27 07:43:48 -0300

    Last touches - icon, loading scene.

---
+++ Assets/Scripts/LoadGame.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadGame : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		StartCoroutine (loadGameScene());
+	}
+
+	IEnumerator loadGameScene () {
+		AsyncOperation async = SceneManager.LoadSceneAsync("Main");
+
+		while (!async.isDone) {
+			yield return null;
+		}
+	}
+}
